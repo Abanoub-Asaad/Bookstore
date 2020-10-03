6a1d301be5a7a968f75202b02088fd5e8fca3ffe
Abanoub-Asaad <abanoub.asaad200010@gmail.com>
2020-10-03 16:21:16 +0200

    Fix errors and attach book details again to the project

--- Bookstore/src/Screens/Home.js
+++ Bookstore/src/Screens/Home.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import BookOverview from "../Components/BookOverview";
 import Icon from "../Components/Icon";
+
 const path = "../../assets/";
 const BookCovers = {
-  firstBook: require(`${path}above-all-else-cover-hires.jpg`),  secondBook: require(`${path}mocktails-hires.jpg`),
-  thirdBook: require(`${path}mothers-journey-hires.jpg`),
-  fourthBook: require(`${path}alice-greta-reissue-hires.jpg`),
+  firstBook: {path:require(`${path}above-all-else-cover-hires.jpg`),title:"above all else"},
+   secondBook:{path:require(`${path}alice-greta-reissue-hires.jpg`),title:"alice greta reissue"},
+  thirdBook: {path:require(`${path}mocktails-hires.jpg`),title:"mocktails"},
+  fourthBook: {path:require(`${path}mothers-journey-hires.jpg`),title:"mothers journey"}
 };
-
+const context=createContext(BookCovers)
 const Home = () => {
+const value=useContext(context)
+
   return (
     <View style={styles.mainview}>
+
       <Icon />
       <View style={styles.shelf}>
-        <BookOverview imgsrc={BookCovers.firstBook} />
-        <BookOverview imgsrc={BookCovers.secondBook} />
-        <BookOverview imgsrc={BookCovers.thirdBook} />
-        <BookOverview imgsrc={BookCovers.fourthBook} />
+        <BookOverview imgsrc={BookCovers.firstBook.path} title={BookCovers.firstBook.title}/>
+        <BookOverview imgsrc={BookCovers.secondBook.path} title={BookCovers.secondBook.title}/>
+        <BookOverview imgsrc={BookCovers.thirdBook.path} title={BookCovers.thirdBook.title}/>
+        <BookOverview imgsrc={BookCovers.fourthBook.path} title={BookCovers.fourthBook.title}/>
       </View>
     </View>
   );
